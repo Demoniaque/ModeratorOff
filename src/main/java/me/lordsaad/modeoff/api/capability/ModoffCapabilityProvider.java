1f3268f686ae6ce619ced102b0a53b5d98b5bc1e
Paul Fulham <paul.fulham0@gmail.com>
2018-01-26 17:03:39 -0800

    Remove superfluous INBTSerializable implementation

--- src/main/java/me/lordsaad/modeoff/api/capability/ModoffCapabilityProvider.java
+++ src/main/java/me/lordsaad/modeoff/api/capability/ModoffCapabilityProvider.java
@@ -3,14 +3,12 @@ package me.lordsaad.modeoff.api.capability;
 import me.lordsaad.modeoff.ModeratorOff;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.nbt.NBTBase;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import net.minecraftforge.common.util.INBTSerializable;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -23,7 +21,7 @@ import java.util.Objects;
  * Created by Saad on 8/16/2016.
  */
 @Mod.EventBusSubscriber(modid = ModeratorOff.MOD_ID)
-public final class ModoffCapabilityProvider implements ICapabilityProvider, INBTSerializable<NBTBase> {
+public final class ModoffCapabilityProvider implements ICapabilityProvider {
 
 	private static final ResourceLocation CAPABILITY_ID = new ResourceLocation(ModeratorOff.MOD_ID, "capabilities");
 
@@ -48,16 +46,6 @@ public final class ModoffCapabilityProvider implements ICapabilityProvider, INBT
 	@Override
 	public <T> T getCapability(@Nonnull Capability<T> capability, EnumFacing facing) {
 		return capability == capability() ? capability().cast(this.capability) : null;
-	}
-
-	@Override
-	public NBTBase serializeNBT() {
-		return capability().writeNBT(capability, null);
-	}
-
-	@Override
-	public void deserializeNBT(NBTBase nbt) {
-		capability().readNBT(capability, null, nbt);
 	}
 
 	@Nullable

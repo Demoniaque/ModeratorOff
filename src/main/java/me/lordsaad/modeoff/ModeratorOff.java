fd04ed96c33f67647788b4fb8c9f9afc389cf526
Demoniaque <saadodi44@gmail.com>
2018-02-02 23:08:55 +0200

    update version to 1.1.1

--- src/main/java/me/lordsaad/modeoff/ModeratorOff.java
+++ src/main/java/me/lordsaad/modeoff/ModeratorOff.java
@@ -25,7 +25,7 @@ public class ModeratorOff {
 
 	public static final String MOD_ID = "modeoff";
 	public static final String MOD_NAME = "ModeratorOff";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.1";
 
 	public static final String CLIENT = "me.lordsaad.modeoff.client.ClientProxy";
 	public static final String SERVER = "me.lordsaad.modeoff.server.ServerProxy";
